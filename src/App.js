50b06f788d60ca6cb6d946a14347093066156269
my1243 <mihiryarra@gmail.com>
2022-07-11 23:45:33 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <>
+            <Router>
+                <Routes>
+                    <Route 
+                        element={
+                            <>
+                                <Navbar/>
+                                <Outlet/>
+                                <Footer/>
+                            </>
+                        }
+                    >
+                    {/* Add other pages same as below just change 'home' tag and its path */}
+                        <Route path='/' element={<Home/>} />
+                    </Route>
+                </Routes>
+            </Router>
+        </>
+    )
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <>
+            <div className="bg-gray-200">
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+                <h1>ldshgfdgkfnjd</h1>
+            </div>
+        </>
+    )
+}
+
+export default Home;
