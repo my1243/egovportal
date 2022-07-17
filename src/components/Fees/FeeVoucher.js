9002f52896e0d0623fb8a86b426557772168ac06
Vraj <vraj.patel4801@gmail.com>
2022-07-17 17:13:24 +0530

    more 4 files

--- src/components/Fees/FeeVoucher.js
+++ src/components/Fees/FeeVoucher.js
@@ -4,14 +4,7 @@ import Voucher from "./Voucher";
 const FeeVoucher = () => {
   return (
     <>
-<<<<<<< HEAD
       <h1 className="res-text mx-2 md:mx-12 m-4 font-bold pb-2">Fees Information<hr className="rounded-full shadow-xl mt-2 pt-2 max-w-[40rem] bg-gradient-to-r from-[#f4510b] to-[#e9d022]" /></h1>
-=======
-      <h1 className="text-4xl font-bold mx-6 my-8 sm:mx-12">
-        Fees Information
-        <hr className="rounded-full shadow-xl mt-2 pt-1 max-w-[40rem] bg-gradient-to-r from-[#f4510b] to-[#e9d022]" />
-      </h1>
->>>>>>> ecce0b6656353a268febbd088cb76480431f2a40
       <div className="rounded-xl overflow-hidden">
         <div className="tableDiv pb-12 sm:pb-24 sm:pt-0 sm:mx-12">
           <table className="table rounded-lg border border-collapse overflow-hidden font-medium w-full shadow-lg text-md sm:text-xl px-2 py-4">
