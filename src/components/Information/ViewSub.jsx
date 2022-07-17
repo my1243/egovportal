b619e57fffa8bd709d71779cbeed6433ad0bff3a
Vraj <vraj.patel4801@gmail.com>
2022-07-17 18:19:37 +0530

    commit app.css and viewsub.jsx

--- src/components/Information/ViewSub.jsx
+++ src/components/Information/ViewSub.jsx
@@ -8,7 +8,7 @@ const ViewSub = () => {
     return (
         <>
             <div>
-                <h1 className="mx-8 md:mx-12 m-4 text-4xl font-bold pb-2">
+                <h1 className="mx-2 md:mx-12 m-4 font-bold pb-2">
                     View Subjects
                     <hr className="rounded-full shadow-xl mt-2 pt-1 max-w-[40rem] bg-gradient-to-r from-[#f4510b]  to-[#e9d022]" />
                 </h1>
@@ -49,54 +49,93 @@ const ViewSub = () => {
             {/* Table view of result of search*/}
 
             <div>
-                <div className="flex flex-col md:flex-row rounded-lg border border-orange-500 bg-orange-200 shadow-md mx-12 mt-20">
-                    <div className="flex flex-col justify-evenly mt-10 mx-12 w-1/2">
-                        <div className="flex justify-between">
-                            <h2>Subject code</h2>
-                            <h2>Info</h2>
-                        </div>
-                        <div className="flex justify-between">
-                            <h2>Name</h2>
-                            <h2>Info</h2>
-                        </div>
-                        <div className="flex justify-between">
-                            <h2>Alias</h2>
-                            <h2>Info</h2>
-                        </div>
-                        <div className="flex justify-between">
-                            <h2>Program</h2>
-                            <h2>Info</h2>
-                        </div>
-                        <div className="flex justify-between">
-                            <h2>Rec Status</h2>
-                            <h2>Info</h2>
-                        </div>
-                        <div className="flex justify-between">
-                            <h2>Session No.</h2>
-                            <h2>Info</h2>
-                        </div>
-                        <div className="flex justify-between">
-                            <h2>Elective</h2>
-                            <h2>Info</h2>
-                        </div>
-                        <div className="flex justify-between">
-                            <h2>Credit</h2>
-                            <h2>Info</h2>
-                        </div>
-                        <div className="flex justify-between">
-                            <h2>ThMinPass1</h2>
-                            <h2>Info</h2>
-                        </div>
-                        <div className="flex justify-between">
-                            <h2>ThMinPass2</h2>
-                            <h2>Info</h2>
-                        </div>
-                        <div className="flex justify-between">
-                            <h2>ThTotal</h2>
-                            <h2>Info</h2>
-                        </div>
-                    </div>
-                    <div className="flex flex-col w-1/2"></div>
+                <div className="tableDiv pb-12 sm:pb-24 sm:pt-0 sm:mx-12">
+                    {/* <table className="table rounded-lg overflow-hidden table-auto w-full h-60 shadow-md"> */}
+                    <table className="table rounded-lg border border-collapse overflow-hidden font-medium w-full shadow-lg text-md sm:text-xl px-2 py-4">
+                        <thead className="bg-orange-500 h-12 text-white">
+                            <tr className="text-white tr">
+                                <th className="p-4 th res-text-th">Subject code</th>
+                                <th className="th res-text-th">Name</th>
+                                <th className="th res-text-th">Alias</th>
+                                <th className="th res-text-th">Program</th>
+                                <th className="th res-text-th">Rec status</th>
+                                <th className="th res-text-th">Session no.</th>
+                                <th className="th res-text-th">Elective</th>
+                                <th className="th res-text-th">Credit</th>
+                                <th className="th res-text-th">ThMinPass1</th>
+                                <th className="th res-text-th">ThMinPass2</th>
+                                <th className="th res-text-th">ThTotal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="tr px-3 py-2 font-medium text-center text-lg cursor-pointer even:bg-[#ffd1b1] bg-orange-300">
+                                <td data-label="Subject code" className="td p-4 rmv-padding res-text-td">IT-709</td>
+                                <td data-label="Name" className="td res-text-td">DBMS</td>
+                                <td data-label="Alias" className="td res-text-td">BSNL</td>
+                                <td data-label="Program" className="td res-text-td">B.Tech</td>
+                                <td data-label="Rec status" className="td res-text-td">hello</td>
+                                <td data-label="Session No." className="td res-text-td">1</td>
+                                <td data-label="Elective" className="td res-text-td">AMP</td>
+                                <td data-label="Credit" className="td res-text-td">5.5</td>
+                                <td data-label="ThMinPass1" className="td res-text-td">34</td>
+                                <td data-label="ThMinPass2" className="td res-text-td">34</td>
+                                <td data-label="ThTotal" className="td res-text-td">70</td>
+                            </tr>
+                            <tr className="tr px-3 py-2 font-medium text-center text-lg cursor-pointer even:bg-[#ffd1b1] bg-orange-300">
+                                <td data-label="Subject code" className="td p-4 rmv-padding res-text-td">IT-709</td>
+                                <td data-label="Name" className="td res-text-td">DBMS</td>
+                                <td data-label="Alias" className="td res-text-td">BSNL</td>
+                                <td data-label="Program" className="td res-text-td">B.Tech</td>
+                                <td data-label="Rec status" className="td res-text-td">hello</td>
+                                <td data-label="Session No." className="td res-text-td">1</td>
+                                <td data-label="Elective" className="td res-text-td">AMP</td>
+                                <td data-label="Credit" className="td res-text-td">5.5</td>
+                                <td data-label="ThMinPass1" className="td res-text-td">34</td>
+                                <td data-label="ThMinPass2" className="td res-text-td">34</td>
+                                <td data-label="ThTotal" className="td res-text-td">70</td>
+                            </tr>
+                            <tr className="tr px-3 py-2 font-medium text-center text-lg cursor-pointer even:bg-[#ffd1b1] bg-orange-300">
+                                <td data-label="Subject code" className="td p-4 rmv-padding res-text-td">IT-709</td>
+                                <td data-label="Name" className="td res-text-td">DBMS</td>
+                                <td data-label="Alias" className="td res-text-td">BSNL</td>
+                                <td data-label="Program" className="td res-text-td">B.Tech</td>
+                                <td data-label="Rec status" className="td res-text-td">hello</td>
+                                <td data-label="Session No." className="td res-text-td">1</td>
+                                <td data-label="Elective" className="td res-text-td">AMP</td>
+                                <td data-label="Credit" className="td res-text-td">5.5</td>
+                                <td data-label="ThMinPass1" className="td res-text-td">34</td>
+                                <td data-label="ThMinPass2" className="td res-text-td">34</td>
+                                <td data-label="ThTotal" className="td res-text-td">70</td>
+                            </tr>
+                            <tr className="tr px-3 py-2 font-medium text-center text-lg cursor-pointer even:bg-[#ffd1b1] bg-orange-300">
+                                <td data-label="Subject code" className="td p-4 rmv-padding res-text-td">IT-709</td>
+                                <td data-label="Name" className="td res-text-td">DBMS</td>
+                                <td data-label="Alias" className="td res-text-td">BSNL</td>
+                                <td data-label="Program" className="td res-text-td">B.Tech</td>
+                                <td data-label="Rec status" className="td res-text-td">hello</td>
+                                <td data-label="Session No." className="td res-text-td">1</td>
+                                <td data-label="Elective" className="td res-text-td">AMP</td>
+                                <td data-label="Credit" className="td res-text-td">5.5</td>
+                                <td data-label="ThMinPass1" className="td res-text-td">34</td>
+                                <td data-label="ThMinPass2" className="td res-text-td">34</td>
+                                <td data-label="ThTotal" className="td res-text-td">70</td>
+                            </tr>
+
+                            <tr className="tr px-3 py-2 font-medium text-center text-lg cursor-pointer even:bg-[#ffd1b1] bg-orange-300">
+                                <td data-label="Subject code" className="td p-4 rmv-padding res-text-td">IT-709</td>
+                                <td data-label="Name" className="td res-text-td">DBMS</td>
+                                <td data-label="Alias" className="td res-text-td">BSNL</td>
+                                <td data-label="Program" className="td res-text-td">B.Tech</td>
+                                <td data-label="Rec status" className="td res-text-td">hello</td>
+                                <td data-label="Session No." className="td res-text-td">1</td>
+                                <td data-label="Elective" className="td res-text-td">AMP</td>
+                                <td data-label="Credit" className="td res-text-td">5.5</td>
+                                <td data-label="ThMinPass1" className="td res-text-td">34</td>
+                                <td data-label="ThMinPass2" className="td res-text-td">34</td>
+                                <td data-label="ThTotal" className="td res-text-td">70</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </>
